b03adbcabf433d48c7ea317bf438a88bb2e93f1a
Colin Nartey <colinnartey123@gmail.com>
2026-04-25 16:40:01 +0100

    Created the journal, profile and settings page xaml files and their corresponding xaml.cs files so that their buttons on the the navigation bar dont just all direct the user to the dashboard page like it originally did. I added skeleton placeholders for the content of each page just so it makes it easier for me to identify which page is which as im not onto developing those pages yet.

---
+++ assessment2526/ProfilePage.xaml.cs
@@ -0,0 +1,10 @@
+namespace assessment2526;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+
+}
---
+++ assessment2526/Settings.xaml.cs
@@ -0,0 +1,10 @@
+namespace assessment2526;
+
+public partial class Settings : ContentPage
+{
+	public Settings()
+	{
+		InitializeComponent();
+	}
+
+}

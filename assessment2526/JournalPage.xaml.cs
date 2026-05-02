3e001f7e5a0ee8bf6d78cc4e1acca3a459460545
Colin Nartey <colinnartey123@gmail.com>
2026-05-02 13:38:11 +0100

    For the biometric security of the journal page, i used the features section of the iOS simulator's menu bar to enroll a fake face which allowed me to simulate successful logins and failed attempts through the matching face and non matching face features to make sure the security logic worked perfectly. I ran into await errors when trying to run the code based off the original password code in the JournalPage.xaml.cs, so i added async to the unlock method to wait for the user to be authenticated before moving onto the next phase. I also had to uninstall the old the fingerprint plugin package and downloaded the MAUI compatible beta to stop the exception error that kept happening when trying to run biometrics.

--- assessment2526/JournalPage.xaml.cs
+++ assessment2526/JournalPage.xaml.cs
@@ -9,8 +9,27 @@ namespace assessment2526
 			InitializeComponent();
 		}
 
-		private void OnUnlockClicked(object sender, EventArgs e)
+		private async void OnUnlockClicked(object sender, EventArgs e)
         {
+
+			bool isAvailable = await CrossFingerprint.Current.IsAvailableAsync();
+
+			if (isAvailable)
+    {
+			var request = new AuthenticationRequestConfiguration("Unlock Journal", "Please prove it's you to read your notes.");
+			var result = await CrossFingerprint.Current.AuthenticateAsync(request);
+
+			if (result.Authenticated)
+			{
+				LockedView.IsVisible = false;
+				UnlockedView.IsVisible = true;
+				PasswordEntry.Text = ""; 
+				return; 
+			}
+    }
+
+
+
             if (PasswordEntry.Text == "ceemxnnytest")
             {
                 LockedView.IsVisible = false;
@@ -20,7 +39,7 @@ namespace assessment2526
             }
             else
             {
-                DisplayAlert("Access Denied", "Incorrect password. Please try again.", "OK");
+                DisplayAlert("Access Denied", "Please try again.", "OK");
             }
         }
 

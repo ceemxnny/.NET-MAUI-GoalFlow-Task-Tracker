d76dd47903bf407aa0474639b9a5f7cb8e819917
Colin Nartey <colinnartey123@gmail.com>
2026-05-03 20:02:00 +0100

    i have now implemented the needed requirements and permissions for location implemtation alongside android fingerprint implementation

--- assessment2526/Dashboard.xaml.cs
+++ assessment2526/Dashboard.xaml.cs
@@ -20,4 +20,27 @@ public partial class Dashboard : ContentPage
 	{
 		await Navigation.PopAsync();
 	}
+
+	protected override async void OnAppearing()
+{
+    base.OnAppearing();
+
+    try
+    {
+        var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+        var location = await Geolocation.Default.GetLocationAsync(request);
+
+        if (location != null)
+        {
+            var mapSpan = new Microsoft.Maui.Maps.MapSpan(location, 0.01, 0.01);
+            
+            DashboardMap.MoveToRegion(mapSpan);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"GPS failed: {ex.Message}");
+    }
+}
+
 }
